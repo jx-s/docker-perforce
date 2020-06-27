76373dce1ae11c97f1d5bbfbd1a13fe5e8b07d9e
fahasiki <sunjianxin0207@gmail.com>
2020-06-27 20:32:21 +0800

    p4server--->2020.1

--- perforce-server/setup-perforce.sh
+++ perforce-server/setup-perforce.sh
@@ -53,6 +53,39 @@ if [ "$FRESHINSTALL" = "1" ]; then
     p4 user -i < /root/p4-users.txt
     p4 group -i < /root/p4-groups.txt
     p4 protect -i < /root/p4-protect.txt
+
+    # disable automatic user account creation
+    p4 configure set lbr.proxy.case=1
+
+    # disable unauthorized viewing of Perforce user list
+    p4 configure set run.users.authorize=1
+
+    # disable unauthorized viewing of Perforce config settings
+    p4 configure set dm.keys.hide=2
+
+    # Update the Typemap
+    # Based on : https://docs.unrealengine.com/en-us/Engine/Basics/SourceControl/Perforce
+    (p4 typemap -o; echo " binary+w //depot/....exe") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....dll") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....lib") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....app") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....dylib") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....stub") | p4 typemap -i
+    (p4 typemap -o; echo " binary+w //depot/....ipa") | p4 typemap -i
+    (p4 typemap -o; echo " binary //depot/....bmp") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....ini") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....config") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....cpp") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....h") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....c") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....cs") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....m") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....mm") | p4 typemap -i
+    (p4 typemap -o; echo " text //depot/....py") | p4 typemap -i
+    (p4 typemap -o; echo " binary+l //depot/....uasset") | p4 typemap -i
+    (p4 typemap -o; echo " binary+l //depot/....umap") | p4 typemap -i
+    (p4 typemap -o; echo " binary+l //depot/....upk") | p4 typemap -i
+    (p4 typemap -o; echo " binary+l //depot/....udk") | p4 typemap -i
 fi
 
 echo "   P4USER=$P4USER (the admin user)"
